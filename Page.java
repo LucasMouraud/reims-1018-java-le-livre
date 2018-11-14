9ec159a33f8c89f23c1c9980d7d2fbf89bfba6a8
Evrard Gaetan <evrardg92@gmail.com>
2018-11-14 11:33:43 +0100

    Ajout du modèle (page) Ajout du Json

---
+++ Page.java
@@ -0,0 +1,36 @@
+public class Page{
+
+	private int id;
+	private String content;
+	private Array nextPages;
+}
+
+	public Page(int id, String content, Array nextPages){
+		this.id = id;
+		this.content = content;
+		this.nextPages = nextPages;
+	}
+
+	public int getId(){
+		return this.id;
+	}
+
+	public void setId(){
+		this.id = id;
+	}
+
+	public String getContent(){
+		return this.content;
+	}
+
+	public void setContent(){
+		this.content = content;
+	}
+
+	public int getArray(){
+		return this.array;
+	}
+
+	public void setArray(){
+		this.array = array;
+	}
