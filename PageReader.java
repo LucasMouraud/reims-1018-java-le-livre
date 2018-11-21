dec85fcc07fa2f70609fabe707169dc577957dd5
Justine Dubois <justine.dubois.pro@gmail.com>
2018-11-21 09:54:13 +0100

    Ajout de commentaires sur PageReader et du fichier jsonsimple.jar

--- PageReader.java
+++ PageReader.java
@@ -7,15 +7,24 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+// Compile : javac -cp .:json-simple-1.1.1.jar Book.java
+// Execute : java -cp .:json-simple-1.1.1.jar Book
+
 public class PageReader{
+
     private final static String JSON_BOOK_PATH = "Livre.json";
+
     public static void readJson(){
+
+        //lire le fichier json
         FileReader jsonFile = null;
         try {
             jsonFile = new FileReader(JSON_BOOK_PATH);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+        //parser le fichier json
         JSONParser parser = new JSONParser();
         Object jsonParsed = null;
         try{
@@ -23,8 +32,11 @@ public class PageReader{
         } catch (ParseException | IOException e) {
             e.printStackTrace();
         }
+
+        // récupérer la racine du fichier
         JSONArray root = (JSONArray) jsonParsed;
         
+        // créer un tableau pour stocker les données
         Page[] pages = new Page[1];
 
         JSONObject page_object = (JSONObject) root.get(0);
